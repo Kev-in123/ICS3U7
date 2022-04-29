f8cd07e3009a85a29bebe86c48452da46ae630c0
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2022-04-29 16:52:15 -0400

    something looked off..

--- src/Session 5/HW_13_1D_Array/SameSum.java
+++ src/Session 5/HW_13_1D_Array/SameSum.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SameSun {
+public class SameSum {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     // initialize 2 arrays
